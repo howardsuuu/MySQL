ae78a8e28a49bc61484e248baf7f1863a782f183
howardsuuu <howardsu666@gmail.com>
2018-12-19 09:22:20 +0800

    Update DB.py

--- DB.py
+++ DB.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    passwd = "howard24755469",
+    passwd = "",
     database = "howarddb"
     )
 
